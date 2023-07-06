308ff71498733ea11e2931b4c26164aef7068117
Ting Liu <tinglliu@bu.edu>
2023-07-06 17:26:00 -0400

    completed some functions in func.py, working on setting up the cli code

--- func.py
+++ func.py
@@ -6,24 +6,53 @@ the users associated with that project
 import pandas as pd #pandas to parse csv
 #import sys #take in sys arg
 
+'''
+import the two csv files
+currently importing copies i made into my own directory
+in the future i should import directly from source so
+the data stays updated
+'''
 user_df = pd.read_csv('projuser.csv')
 pi_df = pd.read_csv('pidb.csv')
 
 user_labels = ['proj', 'user', 'date']
 user_df.columns = user_labels
 
-def print_users(projname, df) #list the user information for projectname Project
+def proj_users(projname): #list the user information for projectname Project
 
-    filtered = df[df['proj'] == projname]
-
-    print(filtered)
-
-def pi_proj(projname, df) #list the projects for which LPI is either the LPI or Admin Contact
-
-    filtered = df[df['proj'] == projname]
+    filtered = user_df[user_df['proj'] == projname]
 
     print(filtered)
+    return filtered
 
+def pi_projs(pi_login): #list the group + projects for which LPI is either the LPI or Admin Contact
+
+    filtered = pi_df[pi_df['login'] == pi_login]
+
+    projects = filtered[['group', 'title']]
+
+    print(projects)
+    return projects
+
+def semester(): #not sure how to determine the semesters active project, but i'm assuming its any project that is currently active.
+    '''
+    working on getting it to print active academic projects, but having problems filtering out the "academic" column
+    currently just returns all currently active projects
+
+    also not sure what info the apps team needs back, so currently returning everything
+    '''
+    activity = (pi_df['status'] == 'active')
+    #academic = (pi_df['academic'] == "")
+
+    filtered = pi_df[activity]
+    pd.set_option('display.max_columns', None)
+    print(filtered.head(1))
+    projects = filtered[['group', 'title']]
+    return projects
+
+#pi_projs('hgong')
+#semester()
+#proj_users('gonglab')
 
 
 
--- projinfo.py
+++ projinfo.py
@@ -5,15 +5,15 @@ going to put all the cli code here
 from argparse import ArgumentParser
 import pandas as pd
 
-'''
+parser = ArgumentParser(description='hello')
+
 user_df = pd.read_csv('projuser.csv')
 pi_df = pd.read_csv('pidb.csv')
 
 user_labels = ['proj', 'user', 'date']
 user_df.columns = user_labels
 
-print(pi_df.tail(20))
-print(user_df.tail(20))
+#print(pi_df.tail(10))
+#print(user_df.tail(10))
 #projname = str(sys.argv[1])
-'''
 
