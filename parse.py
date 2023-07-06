6af1690b40de334d163f797344cac3cf6be261bb
Ting Liu <tinglliu@bu.edu>
2023-07-05 23:33:35 -0400

    parse takes in a command line argument and prints project contact

--- parse.py
+++ parse.py
@@ -1,5 +1,19 @@
-import pandas as pd
+"""
+currently takes in a project name as a
+command line argument and prints out all
+the users associated with that project
+"""
+import pandas as pd #pandas to parse csv
+import sys #take in sys args
 
-df = pd.read_csv('pidb.csv')
+df = pd.read_csv('projuser.csv')
 
-print(df.to_string())
+labels = ['proj', 'user', 'date']
+
+df.columns = labels
+
+projname = str(sys.argv[1])
+
+filtered = df[df['proj'] == projname]
+
+print(filtered)
