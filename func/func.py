b36ed63b58125cab147e1d3d35c87cd44079fda7
Ting Liu <tinglliu@bu.edu>
2023-07-10 15:36:06 -0400

    put the argparse code into a main function

--- func/func.py
+++ func/func.py
@@ -54,39 +54,43 @@ def semester(): #not sure how to determine the semesters active project, but i'm
     return projects
 
 #****************************************************************************************#
-'''
-adding the arg parse code here!
-- use dictionary to map inputs to functions
-'''
-#create parser + define arguments
-parser = ArgumentParser(description='tool to return information about current projects and PIs') #not sure where this description will show up so idk what to put here lol
-parser.add_argument('arg1', help='function name: proj_users, pi_projs, semester')
-parser.add_argument('--optional', help='project name for proj_users, PI name for pi_projs')
+def main():
+    '''
+    adding the arg parse code here!
+    - use dictionary to map inputs to functions
+    '''
+    #create parser + define arguments
+    parser = ArgumentParser(description='tool to return information about current projects and PIs') #not sure where this description will show up so idk what to put here lol
+    parser.add_argument('arg1', help='function name: proj_users, pi_projs, semester')
+    parser.add_argument('--optional', help='project name for proj_users, PI name for pi_projs')
 
-#take in command line arguments
-args = parser.parse_args()
+    #take in command line arguments
+    args = parser.parse_args()
 
-#save the arguments
-function = args.arg1
-print('input function:', function)
+    #save the arguments
+    function = args.arg1
+    print('input function:', function)
 
-if args.optional:
-    func_input = args.optional
-    print('input func input:', func_input)
+    if args.optional:
+        func_input = args.optional
+        print('input func input:', func_input)
 
-#dictionary to map different functions
-functions = {
-    'proj_users': proj_users,
-    'pi_projs': pi_projs,
-    'semester': semester
-}
+    #dictionary to map different functions
+    functions = {
+        'proj_users': proj_users,
+        'pi_projs': pi_projs,
+        'semester': semester
+    }
 
-call = functions.get(function) #use .get() to avoid KeyError
+    call = functions.get(function) #use .get() to avoid KeyError
 
-if call:
-    if 'func_input' in locals(): #check for secondary input
-        call(func_input)
+    if call:
+        if 'func_input' in locals(): #check for secondary input
+            call(func_input)
+        else:
+            call()
     else:
-        call()
-else:
-    print("invalid input")
+        print("invalid input")
+
+if __name__ == '__main__':
+    main()
