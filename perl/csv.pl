1f006339f91865e082a0fc60b0e388040d5a0767
Ting Liu <tinglliu@bu.edu>
2023-11-15 01:13:32 -0500

    reading in hashes differently, working on fixing the functions

--- perl/csv.pl
+++ perl/csv.pl
@@ -2,7 +2,6 @@ use strict;
 my $hash = "#";
 my $user_df = "/projectnb/rcsmetrics/pidb/data/projuser.csv";
 my $pi_df = "/projectnb/rcsmetrics/pidb/data/pidb.csv";
-my %user_data = ();
 my %pi_data = ();
 my $csv = 0;
 if ($ARGV[0] eq "-csv") {
@@ -11,72 +10,94 @@ if ($ARGV[0] eq "-csv") {
 my %properties = ();
 my $host = "";
 
+my %user_hash;
+my %group_hash;
 
-my $index = 0;
 open(M, $user_df) or die "can't open $user_df: $!";
 while (my $line = <M>) {
     chomp $line;
-    next if /^$hash/;
-    #next if /^\s*$/;
+    next if $line =~ /^$hash/;  # Skip lines starting with the specified hash pattern
+    next if $line =~ /^\s*$/;   # Skip empty lines
 
     my ($group, $username, $date) = split(',', $line);
 
-    $user_data{$index}->{username} = $username;    
-    $user_data{$index}->{group} = $group;
-    $user_data{$index}->{date} = $date;
-    $index++;
+    # For user_hash, store groups and dates in a nested hash
+    if (!exists $user_hash{$username}) {
+        $user_hash{$username} = {};
+    }
+    $user_hash{$username}{$group} = $date;
+
+    # For group_hash, store usernames and dates in a nested hash
+    if (!exists $group_hash{$group}) {
+        $group_hash{$group} = {};
+    }
+    $group_hash{$group}{$username} = $date;
 }
 close(M);
 
-my $index = 0;
+
 open(N, $pi_df) or die "can't open $pi_df: $!";
 while (my $line = <N>) {
     chomp $line;
-    next if /^$hash/;
-    #next if /^\s*$/;
+    next if $line =~ /^$hash/;  # Skip lines starting with the specified hash pattern
+    next if $line =~ /^\s*$/;   # Skip empty lines
 
-my (
+    my (
         $group, $title, $pi, $login, $email, $dept, $center, $college, $campus,
         $date, $start, $end, $type, $academic, $coursehistory, $status, $aname,
         $alogin, $dataarchive, $quota, $allocation, $last_approved_allocation, $duorequired
     ) = split(',', $line);
 
-    $pi_data{$index}->{login} = $login;
-    $pi_data{$index}->{group} = $group;
-    $pi_data{$index}->{title} = $title;
-    $pi_data{$index}->{pi} = $pi;
-    $pi_data{$index}->{email} = $email;
-    $pi_data{$index}->{dept} = $dept;
-    $pi_data{$index}->{center} = $center;
-    $pi_data{$index}->{college} = $college;
-    $pi_data{$index}->{campus} = $campus;
-    $pi_data{$index}->{date} = $date;
-    $pi_data{$index}->{start} = $start;
-    $pi_data{$index}->{end} = $end;
-    $pi_data{$index}->{type} = $type;
-    $pi_data{$index}->{academic} = $academic;
-    $pi_data{$index}->{coursehistory} = $coursehistory;
-    $pi_data{$index}->{status} = $status;
-    $pi_data{$index}->{aname} = $aname;
-    $pi_data{$index}->{alogin} = $alogin;
-    $pi_data{$index}->{dataarchive} = $dataarchive;
-    $pi_data{$index}->{quota} = $quota;
-    $pi_data{$index}->{allocation} = $allocation;
-    $pi_data{$index}->{last_approved_allocation} = $last_approved_allocation;
-    $pi_data{$index}->{duorequired} = $duorequired;
-
-    $index++;
+    # Create a hash for each title and store all data in it
+    $pi_data{$title} = {
+        group => $group,
+        pi => $pi,
+        login => $login,
+        email => $email,
+        dept => $dept,
+        center => $center,
+        college => $college,
+        campus => $campus,
+        date => $date,
+        start => $start,
+        end => $end,
+        type => $type,
+        academic => $academic,
+        coursehistory => $coursehistory,
+        status => $status,
+        aname => $aname,
+        alogin => $alogin,
+        dataarchive => $dataarchive,
+        quota => $quota,
+        allocation => $allocation,
+        last_approved_allocation => $last_approved_allocation,
+        duorequired => $duorequired
+    };
 }
 close(N);
 
 #test print user csv
-=begin
-foreach my $index (keys %user_data) {
-    print "Username: $user_data{$index}->{username}\n";
-    print "Group: $user_data{$index}->{group}\n";
-    print "Date: $user_data{$index}->{date}\n";
-    print "\n";
+
+foreach my $username (keys %user_hash) {
+    print "Username: $username\n";
+    foreach my $group (keys %{ $user_hash{$username} }) {
+        my $date = $user_hash{$username}{$group};
+        print "\tGroup: $group, Date: $date\n";
+    }
+    print "\n";  # Just for better readability
 }
+
+=begin
+foreach my $group (keys %group_hash) {
+    print "Group: $group\n";
+    foreach my $username (keys %{ $group_hash{$group} }) {
+        my $date = $group_hash{$group}{$username};
+        print "\tUsername: $username, Date: $date\n";
+    }
+    print "\n";  # Just for better readability
+}
+
+
 
 #test print pi csv
 foreach my $index (keys %pi_data) {
@@ -90,31 +111,34 @@ foreach my $index (keys %pi_data) {
 =cut
 
 sub user_proj { #given username, returns the projects that user is in
-    my ($username, %hash) = @_;
-    my %filtered_data;
+    my ($username) = @_;
 
-    foreach my $key (keys %hash) {
-        #print"$hash{$key}\n";
-        if ($hash{$key}->{username} eq $username) {
-            $filtered_data{$key} = $hash{$key};
-            #print$key;
+    foreach my $group (keys %{ $user_hash{$username} }) {
+        my $date = $user_hash{$username}{$group};
+        print "\tGroup: $group, Date: $date\n";
+    }
+
+
+    return $user_hash{$username}
+}
+
+sub project { #given project name, return info
+    my ($projname) = @_;
+    my %filtered_project;
+    foreach my $key (keys %pi_data) {
+        if ($pi_data{$key}->{title} eq $projname) {
+            $filtered_project{$key} = $pi_data{key};
         }
     }
 
-    foreach my $key (keys %filtered_data) {
-        #print "Username: $filtered_result{$key}->{username}\n";
-        print "Group: $filtered_data{$key}->{group}\n";
-        print "Date: $filtered_data{$key}->{date}\n";
-        print "\n";
+    foreach my $key (keys %filtered_project) {
+
     }
 
-
-    return %filtered_data;
 }
 
-sub user_flag { #
 
-}
+#'group', 'title', 'login', 'alogin', 'dept', 'campus'
 
 sub academic {
     my $active_string = "\"active\"";
@@ -122,17 +146,23 @@ sub academic {
     foreach my $key (keys %pi_data) {
         my $academic_status = $pi_data{$key}->{academic};
         if ($academic_status eq $active_string) {
-            print"hi\n";
             $filtered_academic{$key} = $pi_data{$key};
         }
     }
-
+=begin
     foreach my $key (keys %filtered_academic) {
         print "Group: $filtered_academic{$key}->{group}\n";
-        print "Date: $filtered_academic{$key}->{date}\n";
+        print "Title: $filtered_academic{$key}->{title}\n";
+        print "Login: $filtered_academic{$key}->{login}\n";
+        print "ALogin: $filtered_academic{$key}->{alogin}\n";
+        print "Dept: $filtered_academic{$key}->{dept}\n";
+        print "Campus: $filtered_academic{$key}->{campus}\n";
+        print"\n";
     }
-
+=cut
+    return %filtered_academic;
 }
+
 
 my %academic = academic();
 
@@ -148,9 +178,9 @@ my %academic = academic();
 
 
 #testing region
-=begin
+
 my $input_username = 'tinglliu';
-my %filtered_result = user_proj($input_username, %user_data);
+my %filtered_result = user_proj($input_username);
 
 print"$input_username \n";
 foreach my $key (keys %filtered_result) {
@@ -159,4 +189,3 @@ foreach my $key (keys %filtered_result) {
     print "Data: $filtered_result{$key}->{date}\n";
     print "\n";
 }
-=cut
